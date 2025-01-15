20d0ac3a5a332d68d5b48431d89af1ed728e6266
MirazZim <miraz.zim.38@gmail.com>
2025-01-15 13:19:25 +0600

    Display user Info and Sign Out. so when user exist only on button has show that is log out and when user dont exist then only button that has shown that is Sign In

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,25 +1,59 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import auth from "../Firebase/firebase.init";
+import { useState } from "react";
 
 
 
 const Login = () => {
+    const [user , setUser] = useState(null); 
 
     const provider = new GoogleAuthProvider
 
     const handleGoogleSignIn = () => {
         signInWithPopup(auth, provider)
             .then((result) => {
-                console.log(result)
+                console.log(result.user)
+                setUser(result.user)
             })
             .catch(error => {
                 console.log('ERROR', error)
+                setUser(null)
             })
+    }
+
+    const handleGoogleSignOut = () => {
+        signOut(auth)
+        .then(() => {
+            console.log('Sign Out Done');
+            setUser(null)
+        })
+        .catch(error => console.log(error))
     }
 
     return (
         <div>
-            <button onClick={handleGoogleSignIn}>Loging with google</button>
+            {/* <button onClick={handleGoogleSignIn}>Loging with google</button>
+            <br />
+            <br />
+            <button onClick={handleGoogleSignOut}>Log Out</button> */}
+
+            
+
+            {/* jodi User thake toh Log out out dekhao ar jodi user na thake tahole sign in dekhao */}
+
+            {
+                user ? <button onClick={handleGoogleSignOut}>Log Out</button> : <button onClick={handleGoogleSignIn}>Loging with google</button>
+            }
+
+
+
+            {
+                user && <div>
+                    <h2>Name : {user.displayName}</h2>
+                    <h3>Email : {user.email}</h3>
+                    <img src= {user.photoURL} alt="" />
+                </div>
+            }
         </div>
     );
 };
